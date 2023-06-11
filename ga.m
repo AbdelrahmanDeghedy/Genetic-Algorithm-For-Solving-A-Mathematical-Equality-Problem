8df006630451c7cc8817c925909b08aaa98340f7
Abdelrahman Deghedy <68364202+AbdelrahmanDeghedy@users.noreply.github.com>
2023-06-11 11:33:05 +0300

    Iterating

--- ga.m
+++ ga.m
@@ -14,88 +14,95 @@ Chromosome6 = { 20, 5, 17, 1 };
 Chromosomes = { Chromosome1; Chromosome2; Chromosome3; Chromosome4; Chromosome5; Chromosome6 };
 
 % Create a loop here
+for i = 1 : 50
+    % 2. Evaluation (the fitness function)
+    F_obj = {0, 0, 0, 0, 0, 0};
+    for i = 1 : 6
+        F_obj{i} = objective_function(Chromosomes{i}{1}, Chromosomes{i}{2}, Chromosomes{i}{3}, Chromosomes{i}{4});
+    end
 
-% 2. Evaluation (the fitness function)
-F_obj = {0, 0, 0, 0, 0, 0};
-for i = 1 : 6
-    F_obj{i} = objective_function(Chromosomes{i}{1}, Chromosomes{i}{2}, Chromosomes{i}{3}, Chromosomes{i}{4});
-end
+    % 3. Selection (Select the fittest chromosome)
+    Fitness = {0, 0, 0, 0, 0, 0};
+    totalFitness = 0;
+    for i = 1 : 6
+        Fitness{i} = 1 / (1 + F_obj{i});
+        totalFitness = totalFitness + Fitness{i};
+    end
 
-% 3. Selection (Select the fittest chromosome)
-Fitness = {0, 0, 0, 0, 0, 0};
-totalFitness = 0;
-for i = 1 : 6
-    Fitness{i} = 1 / (1 + F_obj{i});
-    totalFitness = totalFitness + Fitness{i};
-end
+    % 3b. Calculation of the probability of each chromosome
+    P = {0, 0, 0, 0, 0, 0};
+    for i = 1 : 6
+        P{i} = Fitness{i} / totalFitness;
+    end
 
-% 3b. Calculation of the probability of each chromosome
-P = {0, 0, 0, 0, 0, 0};
-for i = 1 : 6
-    P{i} = Fitness{i} / totalFitness;
-end
+    % 3c. Calcultions of the cumulative probabilities
+    C = {P{1}, 0, 0, 0, 0, 0};
+    for i = 2 : 6
+        C{i} = C{i - 1} + P{i};
+    end
 
-% 3c. Calcultions of the cumulative probabilities
-C = {P{1}, 0, 0, 0, 0, 0};
-for i = 2 : 6
-    C{i} = C{i - 1} + P{i};
-end
+    % 3d. Generating random numbers R (from 0 - 1)
+    R = {0, 0, 0, 0, 0, 0};
+    for i = 1 : 6
+        R{i} = rand;
+    end
 
-% 3d. Generating random numbers R (from 0 - 1)
-R = {0, 0, 0, 0, 0, 0};
-for i = 1 : 6
-    R{i} = rand;
-end
+    % 3e. Crossover
+    NewChromosomes = {0, 0, 0, 0, 0, 0};
+    for i = 1 : 6
+        for j = 1 : 6
+            if(R{i} < C{j})
+                NewChromosomes{i} = Chromosomes{j};
+                break;
+            end
+        end
+    end
 
-% 3e. Crossover
-NewChromosomes = {0, 0, 0, 0, 0, 0};
-for i = 1 : 6
-    for j = 1 : 6
-        if(R{i} < C{j})
-            NewChromosomes{i} = Chromosomes{j};
-            break;
+    Chromosomes = NewChromosomes;
+
+    % 3f. Chromosome selection
+    crossover_rate = 0.25;
+    crossoverIndices = [];
+    for i = 1 : 6
+        if(R{i} < crossover_rate)
+            crossoverIndices = [crossoverIndices, i];
+        end
+    end
+
+    % 3g. crossover process
+    for i = 1 : length(crossoverIndices)
+        idx = crossoverIndices(i);
+        if i == length(crossoverIndices)
+            nextIdx = 1;
+        else 
+            nextIdx = crossoverIndices(i + 1);
+        end
+
+        splittingIdx = randi([1, 3]);
+        Chromosomes{idx} = [ Chromosomes{idx}(1 : splittingIdx), Chromosomes{nextIdx}(splittingIdx + 1: chromosome_gens ) ];
+    end
+
+    % 4. Mutation
+    mutation_rate = 0.1;
+    total_gen = chromosome_gens * population;
+    number_of_mutations = round(mutation_rate * total_gen);
+    random_indices_for_mutation = 1 + randi(total_gen, [1, number_of_mutations]) - 1;
+
+    for i = 1 : population
+        for j = 1 : chromosome_gens
+            curr_flattened_idx = i * chromosome_gens + j;
+            if find(curr_flattened_idx == random_indices_for_mutation)
+                newMutatedValue = randi([1, 30]);
+                Chromosomes{i}{j} = newMutatedValue;
+            end
         end
     end
 end
 
-Chromosomes = NewChromosomes;
-
-% 3f. Chromosome selection
-crossover_rate = 0.25;
-crossoverIndices = [];
-for i = 1 : 6
-    if(R{i} < crossover_rate)
-        crossoverIndices = [crossoverIndices, i];
-    end
-end
-
-% 3g. crossover process
-for i = 1 : length(crossoverIndices)
-    idx = crossoverIndices(i);
-    if i == length(crossoverIndices)
-        nextIdx = 1;
-    else 
-        nextIdx = crossoverIndices(i + 1);
-    end
-    
-    splittingIdx = randi([1, 3]);
-    Chromosomes{idx} = [ Chromosomes{idx}(1 : splittingIdx), Chromosomes{nextIdx}(splittingIdx + 1: chromosome_gens ) ];
-end
-
-% 4. Mutation
-mutation_rate = 0.1;
-total_gen = chromosome_gens * population;
-number_of_mutations = round(mutation_rate * total_gen);
-random_indices_for_mutation = 1 + randi(total_gen, [1, number_of_mutations]) - 1;
-
+% Printing the best Chromosomes
 for i = 1 : population
     for j = 1 : chromosome_gens
-        curr_flattened_idx = i * chromosome_gens + j;
-        if find(curr_flattened_idx == random_indices_for_mutation)
-            newMutatedValue = randi([1, 30]);
-            Chromosomes{i}{j} = newMutatedValue;
-        end
+        fprintf('%d ', Chromosomes{i}{j});
     end
+    fprintf('\n');
 end
-
-
