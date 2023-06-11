9b8d04f58c5cd4f42828eadc3d60353a42e1bab3
Raneem Khafagy <69002626+Raneem-Khafagy@users.noreply.github.com>
2023-06-12 01:24:24 +0300

    fixed some errors & Plotted the objective function with iterations
    fixed error:
      same loop variable in the 2nd nested loop F_obj(j)
    
    Stored the best objective function value
    
    Plotted the objective function with iterations

--- ga.m
+++ ga.m
@@ -4,54 +4,55 @@ objective_function = @(a, b, c, d) a + 2 * b + 3 * c + 4 * d - 30;
 % 1. Initialization
 chromosome_gens = 4;
 population = 6;
-Chromosome1 = { 12, 5, 23, 8 };
-Chromosome2 = { 2, 21, 18, 3 };
-Chromosome3 = { 10, 4, 13, 14 };
-Chromosome4 = { 20, 1, 10, 6 };
-Chromosome5 = { 1, 4, 13, 19 };
-Chromosome6 = { 20, 5, 17, 1 };
+Chromosome1 = {12, 5, 23, 8};
+Chromosome2 = {2, 21, 18, 3};
+Chromosome3 = {10, 4, 13, 14};
+Chromosome4 = {20, 1, 10, 6};
+Chromosome5 = {1, 4, 13, 19};
+Chromosome6 = {20, 5, 17, 1};
 
-Chromosomes = { Chromosome1; Chromosome2; Chromosome3; Chromosome4; Chromosome5; Chromosome6 };
+Chromosomes = {Chromosome1; Chromosome2; Chromosome3; Chromosome4; Chromosome5; Chromosome6};
 
 % Create a loop here
-for i = 1 : 50
-    % 2. Evaluation (the fitness function)
-    F_obj = {0, 0, 0, 0, 0, 0};
-    for i = 1 : 6
-        F_obj{i} = objective_function(Chromosomes{i}{1}, Chromosomes{i}{2}, Chromosomes{i}{3}, Chromosomes{i}{4});
-    end
+iterations = 50;
+objective_values = zeros(iterations, 1); % Array to store objective function values
 
+for iteration = 1:iterations
+    % 2. Evaluation (the fitness function)
+    F_obj = zeros(population, 1);
+    for j = 1:population
+        F_obj(j) = objective_function(Chromosomes{j}{1}, Chromosomes{j}{2}, Chromosomes{j}{3}, Chromosomes{j}{4});
+    end
+    
+    % Store the best objective function value
+    objective_values(iteration) = max(F_obj);
+    
     % 3. Selection (Select the fittest chromosome)
-    Fitness = {0, 0, 0, 0, 0, 0};
+    Fitness = zeros(population, 1);
     totalFitness = 0;
-    for i = 1 : 6
-        Fitness{i} = 1 / (1 + F_obj{i});
-        totalFitness = totalFitness + Fitness{i};
+    for i = 1:population
+        Fitness(i) = 1 / (1 + F_obj(i));
+        totalFitness = totalFitness + Fitness(i);
     end
 
     % 3b. Calculation of the probability of each chromosome
-    P = {0, 0, 0, 0, 0, 0};
-    for i = 1 : 6
-        P{i} = Fitness{i} / totalFitness;
-    end
+    P = Fitness / totalFitness;
 
-    % 3c. Calcultions of the cumulative probabilities
-    C = {P{1}, 0, 0, 0, 0, 0};
-    for i = 2 : 6
-        C{i} = C{i - 1} + P{i};
+    % 3c. Calculations of the cumulative probabilities
+    C = zeros(population, 1);
+    C(1) = P(1);
+    for i = 2:population
+        C(i) = C(i - 1) + P(i);
     end
 
     % 3d. Generating random numbers R (from 0 - 1)
-    R = {0, 0, 0, 0, 0, 0};
-    for i = 1 : 6
-        R{i} = rand;
-    end
+    R = rand(population, 1);
 
     % 3e. Crossover
-    NewChromosomes = {0, 0, 0, 0, 0, 0};
-    for i = 1 : 6
-        for j = 1 : 6
-            if(R{i} < C{j})
+    NewChromosomes = cell(population, 1);
+    for i = 1:population
+        for j = 1:population
+            if (R(i) < C(j))
                 NewChromosomes{i} = Chromosomes{j};
                 break;
             end
@@ -62,24 +63,19 @@ for i = 1 : 50
 
     % 3f. Chromosome selection
     crossover_rate = 0.25;
-    crossoverIndices = [];
-    for i = 1 : 6
-        if(R{i} < crossover_rate)
-            crossoverIndices = [crossoverIndices, i];
-        end
-    end
+    crossoverIndices = find(R < crossover_rate);
 
-    % 3g. crossover process
-    for i = 1 : length(crossoverIndices)
+    % 3g. Crossover process
+    for i = 1:length(crossoverIndices)
         idx = crossoverIndices(i);
         if i == length(crossoverIndices)
-            nextIdx = 1;
-        else 
+            nextIdx = crossoverIndices(1);
+        else
             nextIdx = crossoverIndices(i + 1);
         end
 
-        splittingIdx = randi([1, 3]);
-        Chromosomes{idx} = [ Chromosomes{idx}(1 : splittingIdx), Chromosomes{nextIdx}(splittingIdx + 1: chromosome_gens ) ];
+        splittingIdx = randi([1, chromosome_gens - 1]);
+        Chromosomes{idx} = [Chromosomes{idx}(1:splittingIdx), Chromosomes{nextIdx}(splittingIdx + 1:end)];% indexing during crossover 
     end
 
     % 4. Mutation
@@ -88,10 +84,10 @@ for i = 1 : 50
     number_of_mutations = round(mutation_rate * total_gen);
     random_indices_for_mutation = 1 + randi(total_gen, [1, number_of_mutations]) - 1;
 
-    for i = 1 : population
-        for j = 1 : chromosome_gens
-            curr_flattened_idx = i * chromosome_gens + j;
-            if find(curr_flattened_idx == random_indices_for_mutation)
+    for i = 1:population
+        for j = 1:chromosome_gens
+            curr_flattened_idx = (i - 1) * chromosome_gens + j;
+            if ismember(curr_flattened_idx, random_indices_for_mutation)
                 newMutatedValue = randi([1, 30]);
                 Chromosomes{i}{j} = newMutatedValue;
             end
@@ -100,9 +96,15 @@ for i = 1 : 50
 end
 
 % Printing the best Chromosomes
-for i = 1 : population
-    for j = 1 : chromosome_gens
+for i = 1:population
+    for j = 1:chromosome_gens
         fprintf('%d ', Chromosomes{i}{j});
     end
     fprintf('\n');
 end
+
+% Plot the objective function with iterations
+plot(1:iterations, objective_values);
+xlabel('Iterations');
+ylabel('Objective Function Value');
+title('Objective Function Convergence');
